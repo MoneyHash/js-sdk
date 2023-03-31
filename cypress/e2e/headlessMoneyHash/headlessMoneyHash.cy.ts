d01ce3a8ae72a0aa3483f42df54d073e9e0e4198
Mo'men Sherif <momensherif.2019@gmail.com>
2023-03-31 20:20:06 +0200

    test: getIntentMethods

--- cypress/e2e/headlessMoneyHash/headlessMoneyHash.cy.ts
+++ cypress/e2e/headlessMoneyHash/headlessMoneyHash.cy.ts
@@ -33,4 +33,46 @@ describe("headlessMoneyHash", () => {
       });
     });
   });
+
+  it.only("moneyHash.getIntentMethods should response with appropriate methods, saved cards and wallet details", () => {
+    cy.createIntent("payment").then(intentId => {
+      cy.getMoneyHashInstance({ type: "payment" }).then(async moneyHash => {
+        const { paymentMethods, expressMethods, savedCards, customerBalances } =
+          await moneyHash.getIntentMethods(intentId);
+
+        expect(paymentMethods).to.be.an("array");
+        expect(expressMethods).to.be.an("array");
+
+        [...paymentMethods, ...expressMethods].forEach(method => {
+          expect(method.id).to.be.a("string");
+          expect(method.title).to.be.a("string");
+          expect(method.confirmationRequired).to.be.a("boolean");
+          expect(method.isSelected).to.be.a("boolean");
+          expect(method.icons).to.be.an("array");
+        });
+
+        expect(savedCards).to.be.an("array");
+
+        savedCards.forEach(card => {
+          expect(card.id).to.be.a("string");
+          expect(card.brand).to.be.a("string");
+          expect(card.expiryMonth).to.be.a("string");
+          expect(card.expiryYear).to.be.a("string");
+          expect(card.last4).to.be.a("string");
+          expect(card.logo).to.be.a("string");
+          expect(card.country).satisfies(
+            country => country === null || typeof country === "string",
+          );
+        });
+
+        expect(customerBalances).to.be.an("array").that.has.length(1);
+        customerBalances.forEach(balance => {
+          expect(balance.id).to.be.a("string");
+          expect(balance.icon).to.be.a("string");
+          expect(balance.balance).to.be.a("number");
+          expect(balance.isSelected).to.be.a("boolean");
+        });
+      });
+    });
+  });
 });
