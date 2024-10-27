9f16c835959f7d21aa39353130155f6dd2e819f5
Mo'men Sherif <momensherif.2019@gmail.com>
2024-10-27 20:49:43 +0300

    chore: change payload structure

--- src/headlessMoneyHash.ts
+++ src/headlessMoneyHash.ts
@@ -469,7 +469,7 @@ export default class MoneyHashHeadless<TType extends IntentType> {
           payload: {
             intentId,
             lang: this.sdkEmbed.lang,
-            receipt: JSON.stringify({ token: e.payment.token }),
+            receipt: e.payment.token,
             receiptBillingData: {
               email: e.payment.shippingContact?.emailAddress,
             },
